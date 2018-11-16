03850db2aedddabf0fdf7f463eef110ba8eeffe0
Shruti <shrutir@uw.edu>
2018-11-15 23:48:17 -0800

    ps77 done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Projects } from './Projects';
+import { Skills } from './Skills';
+import { Homepage } from './Homepage';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+              <div className="container">
+                <Link to="/">Home</Link>
+                { ' ' }
+                <Link to="/projects">Projects</Link>
+                { ' ' }
+                <Link to="/skills">Skills</Link>
+                <Route exact path="/" component={ Homepage } />
+                <Route path="/projects" component={ Projects } />
+                <Route path="/skills" component={ Skills } />
+              </div>
+            </Router>
+            );
+    }
 }
 
 export default App;
---
+++ src/Homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class Homepage extends Component {
+    render() {
+        return <div id="about"><h1>Shruti Rajagopalan</h1>
+            <img src="img/shruti.jpg" alt="a picture of me" />
+
+            <p>Hi there. My name is Shruti and I am currently a junior majoring in Informatics at the University of Washington (UW).
+                I am interested in User Experience and Interface Design. At UW
+                I am a Resident Advisor (RA) for the Global Experience living learning community.
+                I am Director of Finance for an Informatics organization called SUDO and an International student mentor.</p>
+           
+        </div>
+
+    }
+}
