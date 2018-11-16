9cecf421e8d1ec56f616eee2d15e8232e38948c6
Shruti <shrutir@uw.edu>
2018-11-16 01:34:25 -0800

    Updates

---
+++ precache-manifest.60164aebdc4b64412dcf7423c640602b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f18a747e76f14bdbd5b",
+    "url": "/shrutir/ps7/static/js/runtime~main.2f18a747.js"
+  },
+  {
+    "revision": "0ce3ea327c3472804746",
+    "url": "/shrutir/ps7/static/js/main.0ce3ea32.chunk.js"
+  },
+  {
+    "revision": "3d56898b4b8391aed648",
+    "url": "/shrutir/ps7/static/js/1.3d56898b.chunk.js"
+  },
+  {
+    "revision": "0ce3ea327c3472804746",
+    "url": "/shrutir/ps7/static/css/main.99dfbaf5.chunk.css"
+  },
+  {
+    "revision": "3d56898b4b8391aed648",
+    "url": "/shrutir/ps7/static/css/1.1fc88318.chunk.css"
+  },
+  {
+    "revision": "8d3dc93a2dd8fdc70127223b87c2459d",
+    "url": "/shrutir/ps7/index.html"
+  }
+];
